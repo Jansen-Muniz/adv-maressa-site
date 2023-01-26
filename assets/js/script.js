e981f0faecca4a764ea475f0fb4295db1b0c8bf4
Jansen-Muniz <jansen.muniz1@gmail.com>
2023-01-26 16:01:23 -0300

    correção script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,10 @@ const menuMobileButton = document.querySelector('[data-js="botao-menu-responsivo
 menuMobileButton.addEventListener('click', () => {
   if (menuMobile.classList.contains('abrir')) {
     menuMobile.classList.remove('abrir')
-    icon.setAttribute('src', 'assets/svg/menu_white_36dp.svg')
+    icon.setAttribute('src', './svg/menu_white_36dp.svg')
     return
   }
 
   menuMobile.classList.add('abrir')
-  icon.setAttribute('src', 'assets/svg/close_white_36dp.svg')
+  icon.setAttribute('src', './svg/close_white_36dp.svg')
 })
